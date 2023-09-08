5147fb33299b3529b8a1d5e9258f4600b8924ce0
Kevin.Wang <kevin.wang@gosaico.com>
2023-09-08 18:58:55 +0800

    [ongoing]: still solving

--- 1853B.py
+++ 1853B.py
@@ -13,9 +13,6 @@
     a1 >= a0
 """
 
-import sys
-sys.setrecursionlimit(10**9)
-
 def fib2(n):
     if n == 1: return 1, 0
     fn, fn_minus_1 = fib2(n-1)
@@ -27,7 +24,7 @@ def gen_fib(a0, a1, k):
     return [a0 + a1] + gen_fib(a1, a0+a1, k-1)
 
 def problem():
-    n, k = [int(_) for _ in input().strip().split()]
+    n, k = [int(_) for _ in "200000 84".strip().split()]
     c1, c0 = fib2(k-1)
 
     cand = []
@@ -45,6 +42,17 @@ def problem():
     #     print(" ".join([str(int(_)) for _ in ans]))
 
 
-t = int(input())
-for _ in range(t):
-    problem()
+# t = int(input())
+# for _ in range(t):
+#     problem()
+
+
+# 8
+# 22 4
+# 3 9
+# 55 11
+# 42069 6
+# 69420 4
+# 69 1434
+# 1 3
+# 1 4
