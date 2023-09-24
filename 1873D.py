226705ba84286fec1d0285afc48418a1dedb347a
umiumikame <umiumikame@you.never.know>
2023-09-24 17:45:52 +0800

    [feature]: add D v3 (regex)

--- 1873D.py
+++ 1873D.py
@@ -14,23 +14,33 @@ from time import time
 
 import sys
 input = sys.stdin.readline
+import re
 for _ in range(int(input())):
     n, k = map(int, input().split())
-    cells = input().strip()
-    a = time()
+    cells = input().strip().strip("W")
+    cells += "W"*k
     count = 0
-    idx = cells.find("B")
-    if idx == -1:
-        idx = len(cells)
-    while idx < len(cells):
-        idx += k
-        count += 1
-        nxt = cells[idx:].find("B")
-        if nxt == -1:
-            break
-        idx += nxt
-        
-    b = time()
-    print("AAAAAAAAA", count)
+    aa = re.findall(f"B[WB]{{{k-1},{k-1}}}", cells)
+    count = len(aa)
+    print(count)
 
-    print(b-a)
+
+# from time import time
+
+# import sys
+# input = sys.stdin.readline
+# import re
+# for _ in range(int(input())):
+#     n, k = map(int, input().split())
+#     cells = input().strip().strip("W")
+#     rev = cells[::-1]
+#     a = time()
+#     count = 0
+#     for regex in re.split(f"W{{{k-1},}}", cells):
+#         count += len(regex)//k + (len(regex)%k!=0)
+        
+        
+#     b = time()
+#     print("AAAAAAAAA", count)
+
+#     print(b-a)
