de67efdf57ad7767b9a15adbce94beb9c2a41b67
umiumikame <umiumikame@you.never.know>
2023-09-24 17:50:13 +0800

    [feature]: add template

---
+++ .py-template.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+"""
+@File:
+    .py-template.py
+@Time:
+    2023/09/24 17:49:27
+@Author:
+    Kevin Wang
+@Desc:
+    {{HTML}}}
+    .input-template | .py-template.py
+"""
+
+from time import time
+
+import sys
+input = sys.stdin.readline
+for _ in range(int(input())):
+    n, k = map(int, input().split())
